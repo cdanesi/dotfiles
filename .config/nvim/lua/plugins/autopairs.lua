dff04fb894aeedc5f02f32cb1e117c6f17d3d13f
Charles D <charles@charlesdanesi.net>
2023-06-05 14:49:07 -0400

    small nvim config tweaks

--- .config/nvim/lua/plugins/autopairs.lua
+++ .config/nvim/lua/plugins/autopairs.lua
@@ -3,5 +3,12 @@ if not autopairs_setup then
    return
 end
 
-autopairs.setup()
+autopairs.setup({
+   check_ts = true,
+   ts_config = {
+      lua = { "string" },
+      javascript = { "template_string" },
+      java = false,
+   },
+})
 
