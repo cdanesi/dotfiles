58ba41046bcfd218b1f3656fde8a84074b6c515b
Charles Danesi <charles@charlesdanesi.net>
2024-11-01 14:26:18 -0400

    add session indicator to lualine

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,59 +1,66 @@
 return {
    "nvim-lualine/lualine.nvim",
-   dependencies = { 
+   dependencies = {
       "nvim-tree/nvim-web-devicons",
-      "rmehri01/onenord.nvim"
+      "rmehri01/onenord.nvim",
    },
    config = function()
-      local lualine = require("lualine")
-      local lazy_status = require("lazy.status")
+      local lualine = require "lualine"
+      local lazy_status = require "lazy.status"
 
-      lualine.setup({
-        options = {
-          icons_enabled = true,
-          theme = 'onenord',
-          component_separators = { left = '', right = ''},
-          section_separators = { left = '', right = ''},
-          disabled_filetypes = {
-            statusline = {},
-            winbar = {},
-          },
-          ignore_focus = {},
-          always_divide_middle = true,
-          globalstatus = false,
-          refresh = {
-            statusline = 1000,
-            tabline = 1000,
-            winbar = 1000,
-          }
-        },
-        sections = {
-          lualine_a = {'mode'},
-          lualine_b = {'branch', 'diff', 'diagnostics'},
-          lualine_c = {'filename'},
-          lualine_x = {
-             {
-                lazy_status.updates,
-                cond = lazy_status.has_updates,
-                color = { fg = "#ff9e64" },
-             },
-             {'encoding', 'fileformat', 'filetype'},
-          },
-          lualine_y = {'progress'},
-          lualine_z = {'location'}
-        },
-        inactive_sections = {
-          lualine_a = {},
-          lualine_b = {},
-          lualine_c = {'filename'},
-          lualine_x = {'location'},
-          lualine_y = {},
-          lualine_z = {}
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-      })
+      lualine.setup {
+         options = {
+            icons_enabled = true,
+            theme = "onenord",
+            component_separators = { left = "", right = "" },
+            section_separators = { left = "", right = "" },
+            disabled_filetypes = {
+               statusline = {},
+               winbar = {},
+            },
+            ignore_focus = {},
+            always_divide_middle = true,
+            globalstatus = false,
+            refresh = {
+               statusline = 1000,
+               tabline = 1000,
+               winbar = 1000,
+            },
+         },
+         sections = {
+            lualine_a = { "mode" },
+            lualine_b = { "branch", "diff", "diagnostics" },
+            lualine_c = {
+               {
+                  function()
+                     return require("auto-session.lib").current_session_name(true)
+                  end,
+               },
+               { "filename" },
+            },
+            lualine_x = {
+               {
+                  lazy_status.updates,
+                  cond = lazy_status.has_updates,
+                  color = { fg = "#ff9e64" },
+               },
+               { "encoding", "fileformat", "filetype" },
+            },
+            lualine_y = { "progress" },
+            lualine_z = { "location" },
+         },
+         inactive_sections = {
+            lualine_a = {},
+            lualine_b = {},
+            lualine_c = { "filename" },
+            lualine_x = { "location" },
+            lualine_y = {},
+            lualine_z = {},
+         },
+         tabline = {},
+         winbar = {},
+         inactive_winbar = {},
+         extensions = {},
+      }
    end,
 }
