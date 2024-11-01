600f4042c78348e6d5314a066086080525c72cf7
Charles Danesi <charles@charlesdanesi.net>
2024-11-01 14:28:15 -0400

    add trouble to telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -8,23 +8,33 @@ return {
       "folke/todo-comments.nvim",
    },
    config = function()
-      local telescope = require("telescope")
-      local actions = require("telescope.actions")
+      local telescope = require "telescope"
+      local actions = require "telescope.actions"
+      local transform_mod = require("telescope.actions.mt").transform_mod
+      local trouble = require "trouble"
+      local trouble_telescope = require "trouble.sources.telescope"
 
-      telescope.setup({
+      local custom_actions = transform_mod {
+         open_trouble_qflist = function(prompt_bufnr)
+            trouble.toggle "quickfix"
+         end,
+      }
+
+      telescope.setup {
          defaults = {
             path_display = { "smart" },
             mappings = {
                i = {
                   ["<C-k>"] = actions.move_selection_previous,
                   ["<C-j>"] = actions.move_selection_next,
-                  ["<C-q>"] = actions.send_selected_to_qflist + actions.open_qflist,
+                  ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+                  ["<C-t>"] = trouble_telescope.open,
                },
             },
          },
-      })
+      }
 
-      telescope.load_extension("fzf")
+      telescope.load_extension "fzf"
 
       -- keymaps
       local keymap = vim.keymap
