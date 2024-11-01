a9711ba2e0f1dfa16ca291afda2da08b7c593bd0
Charles Danesi <charles@charlesdanesi.net>
2024-11-01 14:27:17 -0400

    change folder icons

--- .config/nvim/lua/plugins/nvim-tree.lua
+++ .config/nvim/lua/plugins/nvim-tree.lua
@@ -2,12 +2,12 @@ return {
    "nvim-tree/nvim-tree.lua",
    dependencies = "nvim-tree/nvim-web-devicons",
    config = function()
-      local nvimtree = require("nvim-tree")
+      local nvimtree = require "nvim-tree"
 
       vim.g.loaded_netrw = 1
       vim.g.loaded_netrwPlugin = 1
 
-      nvimtree.setup({
+      nvimtree.setup {
          view = {
             width = 35,
             relativenumber = true,
@@ -18,9 +18,14 @@ return {
             },
             icons = {
                glyphs = {
+                  default = "",
                   folder = {
-                     arrow_closed = "", -- arrow when folder is closed
-                     arrow_open = "", -- arrow when folder is open
+                     default = "",
+                     open = "",
+                     empty = "",
+                     empty_open = "",
+                     arrow_closed = "", -- arrow when folder is closed
+                     arrow_open = "", -- arrow when folder is open
                   },
                },
             },
@@ -38,13 +43,18 @@ return {
          git = {
             ignore = false,
          },
-      })
+      }
 
       local keymap = vim.keymap
 
       keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
-      keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" })
+      keymap.set(
+         "n",
+         "<leader>ef",
+         "<cmd>NvimTreeFindFileToggle<CR>",
+         { desc = "Toggle file explorer on current file" }
+      )
       keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
       keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
-   end
+   end,
 }
