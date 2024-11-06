d20485daf54fa17e85ce1a4f608b7f54c6e93bf2
Charles Danesi <charles@charlesdanesi.net>
2024-11-05 20:12:35 -0500

    add nifty styling to completion menu
    added source name to completion menu
    kind-specific colorizing in the completion menu

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -22,6 +22,10 @@ return {
       require("luasnip.loaders.from_vscode").lazy_load()
 
       cmp.setup {
+         view = {
+            entries = "custom",
+            selection_order = "near_cursor",
+         },
          completion = {
             completeopt = "menu,menuone,preview,noselect",
          },
@@ -47,13 +51,32 @@ return {
          },
          formatting = {
             format = lspkind.cmp_format {
-               mode = "text",
-               preset = "codeicons",
+               mode = "symbol_text",
                maxwidth = 50,
                ellipsis_char = "...",
-               show_labelDetails = false,
+               show_labelDetails = true,
+               menu = {
+                  -- add name of source to the menu
+                  nvim_lsp = "[LSP]",
+                  nvim_lua = "[Lua]",
+                  luasnip = "[LuaSnip]",
+                  buffer = "[Buffer]",
+                  path = "[Path]",
+               },
             },
          },
+         -- VSCode-like, kind specific highlights
+         vim.api.nvim_set_hl(0, "CmpItemAbbrDeprecated", { bg = "NONE", strikethrough = true, fg = "#808080" }),
+         vim.api.nvim_set_hl(0, "CmpItemAbbrMatch", { bg = "NONE", fg = "#569CD6" }),
+         vim.api.nvim_set_hl(0, "CmpItemAbbrMatchFuzzy", { link = "CmpIntemAbbrMatch" }),
+         vim.api.nvim_set_hl(0, "CmpItemKindVariable", { bg = "NONE", fg = "#9CDCFE" }),
+         vim.api.nvim_set_hl(0, "CmpItemKindInterface", { link = "CmpItemKindVariable" }),
+         vim.api.nvim_set_hl(0, "CmpItemKindText", { link = "CmpItemKindVariable" }),
+         vim.api.nvim_set_hl(0, "CmpItemKindFunction", { bg = "NONE", fg = "#C586C0" }),
+         vim.api.nvim_set_hl(0, "CmpItemKindMethod", { link = "CmpItemKindFunction" }),
+         vim.api.nvim_set_hl(0, "CmpItemKindKeyword", { bg = "NONE", fg = "#D4D4D4" }),
+         vim.api.nvim_set_hl(0, "CmpItemKindProperty", { link = "CmpItemKindKeyword" }),
+         vim.api.nvim_set_hl(0, "CmpItemKindUnit", { link = "CmpItemKindKeyword" }),
       }
    end,
 }
