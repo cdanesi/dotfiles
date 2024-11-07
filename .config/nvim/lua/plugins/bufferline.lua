19f1e405a93102a28890f104305fde751f6ddc4f
Charles Danesi <charles@charlesdanesi.net>
2024-11-07 15:30:29 -0500

    bufferline config
    tried to offset the tab list when the file tree is showing, but that
    either doesn't work or I don't undertstand something

--- .config/nvim/lua/plugins/bufferline.lua
+++ .config/nvim/lua/plugins/bufferline.lua
@@ -6,6 +6,21 @@ return {
       options = {
          mode = "tabs",
          separator_style = "slant",
+         diagnostics = "nvim_lsp",
+         always_show_bufferline = false,
+         show_tab_indicators = true,
+         color_icons = true,
+         show_buffer_icons = true,
+         persist_buffer_sort = true,
+         indicator = {
+            style = "underline",
+         },
+         offsets = {
+            filetype = "NvimTree",
+            text = "File Explorer",
+            text_align = "left",
+            separator = true,
+         },
       },
    },
 }
