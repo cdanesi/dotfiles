921f0b307b8d66b8a3e747967eef853d5247bbc5
Charles Danesi <charles@charlesdanesi.net>
2024-11-16 09:05:03 -0500

    add auto color adjustment to make devicons look a bit nicer with themes

---
+++ .config/nvim/lua/plugins/devicons-auto-colors.lua
@@ -0,0 +1,22 @@
+return {
+   "rachartier/tiny-devicons-auto-colors.nvim",
+   dependencies = {
+      "nvim-tree/nvim-web-devicons",
+   },
+   event = "VeryLazy",
+
+   config = function()
+      require("tiny-devicons-auto-colors").setup {
+         cache = {
+            enabled = true,
+            path = vim.fn.stdpath "cache" .. "/tiny-devicons-auto-colors-cache.json",
+         },
+         precise_search = {
+            enabled = true,
+            iteration = 10,
+            precision = 20,
+            threshold = 23,
+         },
+      }
+   end,
+}
