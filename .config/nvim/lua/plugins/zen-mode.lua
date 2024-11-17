6d1e26768e9c289ce9664ad4e982a124471cb768
Charles Danesi <charles@charlesdanesi.net>
2024-11-17 18:12:31 -0500

    add zen mode + twilight for writing

---
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -0,0 +1,17 @@
+return {
+   "folke/zen-mode.nvim",
+   opts = {
+      window = { signcolumn = true },
+      plugins = {
+         options = { enabled = true },
+         twilight = { enabled = true },
+         gitsigns = { enabled = true },
+         tmux = { enabled = true },
+         todo = { enabled = true },
+         alacritty = {
+            enabled = true,
+            font = "16",
+         },
+      },
+   },
+}
