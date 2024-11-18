55d4caf129e65aea4a6131eca7f0079220308ac1
Charles Danesi <charles@charlesdanesi.net>
2024-11-18 09:59:10 -0500

    colorize indent lines

--- .config/nvim/lua/plugins/indent-blankline.lua
+++ .config/nvim/lua/plugins/indent-blankline.lua
@@ -3,6 +3,41 @@ return {
    event = { "BufReadPre", "BufNewFile" },
    main = "ibl",
    opts = {
-      indent = { char = "┊" },
+      indent = {
+         char = "┊",
+         smart_indent_cap = true,
+         highlight = {
+            -- indent coloring for nord theme
+            -- FIX: set these up manually?
+            "Headline6",
+            "Headline5",
+            "Headline4",
+            "Headline3",
+            "Headline2",
+            "Headline1",
+         },
+      },
+      scope = {
+         enabled = true,
+         show_start = true,
+         show_end = true,
+         -- highlight = { "Function", "Label" },
+         char = "▎",
+      },
+      exclude = {
+         filetypes = {
+            "lspinfo",
+            "checkhealth",
+            "help",
+            "man",
+            "gitcommit",
+            "TelescopePrompt",
+            "TelescopeResults",
+            "",
+            "dashboard",
+            "alpha",
+            "NvimTree",
+         },
+      },
    },
 }
