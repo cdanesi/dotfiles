0a99df6d9e9854f012221948602eb75be7944e8b
Charles Danesi <charles@charlesdanesi.net>
2024-11-20 09:49:29 -0500

    add disabled.lua for testing

---
+++ .config/nvim/lua/plugins/disabled.lua
@@ -0,0 +1,22 @@
+return {
+   {
+      "tadmccorkle/markdown.nvim",
+      enabled = false,
+   },
+   {
+      "OXY2DEV/markview.nvim",
+      enabled = false,
+   },
+   {
+      "jakewvincent/mkdnflow.nvim",
+      enabled = false,
+   },
+   {
+      "willothy/moveline.nvim",
+      enabled = false,
+   },
+   {
+      "MeanderingProgrammer/render-markdown.nvim",
+      enabled = false,
+   },
+}
