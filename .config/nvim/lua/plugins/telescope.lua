255a6011f0592c721ee16069972aa5ae71729154
Charles Danesi <charles@charlesdanesi.net>
2024-11-21 12:40:01 -0500

    add keybind to delete buffer

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,39 +1,44 @@
 return {
-   "nvim-telescope/telescope.nvim",
-   branch = "0.1.x",
-   dependencies = {
-      "nvim-lua/plenary.nvim",
-      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
-      "nvim-tree/nvim-web-devicons",
-      "folke/todo-comments.nvim",
-   },
-   config = function()
-      local telescope = require "telescope"
-      local actions = require "telescope.actions"
-      local transform_mod = require("telescope.actions.mt").transform_mod
-      local trouble = require "trouble"
-      local trouble_telescope = require "trouble.sources.telescope"
+  "nvim-telescope/telescope.nvim",
+  branch = "0.1.x",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    "nvim-tree/nvim-web-devicons",
+    "folke/todo-comments.nvim",
+  },
+  config = function()
+    local telescope = require("telescope")
+    local actions = require("telescope.actions")
+    local transform_mod = require("telescope.actions.mt").transform_mod
+    local trouble = require("trouble")
+    local trouble_telescope = require("trouble.sources.telescope")
 
-      local custom_actions = transform_mod {
-         open_trouble_qflist = function(prompt_bufnr)
-            trouble.toggle "quickfix"
-         end,
-      }
+    local custom_actions = transform_mod({
+      open_trouble_qflist = function(prompt_bufnr)
+        trouble.toggle("quickfix")
+      end,
+    })
 
-      telescope.setup {
-         defaults = {
-            path_display = { "smart" },
-            mappings = {
-               i = {
-                  ["<C-k>"] = actions.move_selection_previous,
-                  ["<C-j>"] = actions.move_selection_next,
-                  ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
-                  ["<C-t>"] = trouble_telescope.open,
-               },
-            },
-         },
-      }
+    telescope.setup({
+      defaults = {
+        path_display = { "smart" },
+        mappings = {
+          n = {
+            ["x"] = actions.delete_buffer,
+            ["q"] = actions.close,
+          },
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+            ["<C-q>"] = actions.send_selected_to_qflist + custom_actions.open_trouble_qflist,
+            ["<C-t>"] = trouble_telescope.open,
+            ["<ESC>"] = actions.close,
+          },
+        },
+      },
+    })
 
-      telescope.load_extension "fzf"
-   end,
+    telescope.load_extension("fzf")
+  end,
 }
