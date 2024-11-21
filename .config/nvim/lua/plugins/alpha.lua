821d04cb2b90cc105a05452fc085ed844518072d
Charles Danesi <charles@charlesdanesi.net>
2024-11-21 09:58:40 -0500

    improved menu options

--- .config/nvim/lua/plugins/alpha.lua
+++ .config/nvim/lua/plugins/alpha.lua
@@ -1,11 +1,11 @@
 return {
    "goolord/alpha-nvim",
    event = "VimEnter",
-   config = function()
-      local alpha = require "alpha"
-      local dashboard = require "alpha.themes.dashboard"
 
-      -- Set header
+   config = function()
+      local alpha = require("alpha")
+      local dashboard = require("alpha.themes.dashboard")
+
       dashboard.section.header.val = {
          " @@@@@@@   @@@@@@@@   @@@@@@@   @@@@@@@@  @@@@@@@@   @@@@@@   @@@@@@@  @@@  @@@  @@@@@@@@  @@@@@@@   ",
          "@@@@@@@@  @@@@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@@  @@@@@@@  @@@  @@@  @@@@@@@@  @@@@@@@@  ",
@@ -20,19 +20,19 @@ return {
          "                                                                                                     ",
       }
 
-      -- Set menu
       dashboard.section.buttons.val = {
-         dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
-         dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
-         dashboard.button("SPC ff", "󰱼 > Find File", "<cmd>Telescope find_files<CR>"),
-         dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
-         dashboard.button("SPC wl", "󰁯  > Restore a Previous Session", "<cmd>SessionSearch<CR>"),
-         dashboard.button("q", " > Quit NVIM", "<cmd>qa<CR>"),
+         dashboard.button("n", "  > New File", "<cmd>ene<CR>"),
+         dashboard.button("e", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
+         dashboard.button("o", "󰙰  > Recent files", "<cmd>Telescope oldfiles<cr>"),
+         dashboard.button("f", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
+         dashboard.button("s", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
+         dashboard.button("r", "󰑓  > Open last session", "<cmd>SessionRestore<CR>"),
+         dashboard.button("R", "󰁯  > Restore a Previous Session", "<cmd>SessionSearch<CR>"),
+         dashboard.button("q", "  > Quit NVIM", "<cmd>qa<CR>"),
       }
 
       alpha.setup(dashboard.opts)
 
-      -- Disable folding on alpha buffer
-      vim.cmd [[autocmd FileType alpha setlocal nofoldenable]]
+      vim.cmd([[autocmd FileType alpha setlocal nofoldenable]])
    end,
 }
