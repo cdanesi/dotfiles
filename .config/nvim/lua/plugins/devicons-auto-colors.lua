1b1222be02a6ef0fa034bbc8e40183069d224a66
Charles Danesi <charles@charlesdanesi.net>
2024-11-22 15:02:27 -0500

    override devicons stuff

--- .config/nvim/lua/plugins/devicons-auto-colors.lua
+++ .config/nvim/lua/plugins/devicons-auto-colors.lua
@@ -1,22 +1,20 @@
 return {
-   "rachartier/tiny-devicons-auto-colors.nvim",
-   dependencies = {
-      "nvim-tree/nvim-web-devicons",
-   },
-   event = "VeryLazy",
+  'rachartier/tiny-devicons-auto-colors.nvim',
+  lazy = true,
 
-   config = function()
-      require("tiny-devicons-auto-colors").setup {
-         cache = {
-            enabled = true,
-            path = vim.fn.stdpath "cache" .. "/tiny-devicons-auto-colors-cache.json",
-         },
-         precise_search = {
-            enabled = true,
-            iteration = 10,
-            precision = 20,
-            threshold = 23,
-         },
-      }
-   end,
+  config = function()
+    require('tiny-devicons-auto-colors').setup({
+      cache = {
+        enabled = true,
+        path = vim.fn.stdpath('cache') .. '/tiny-devicons-auto-colors-cache.json',
+      },
+      precise_search = {
+        enabled = true,
+        iteration = 10,
+        precision = 20,
+        threshold = 23,
+      },
+      autoreload = true,
+    })
+  end,
 }
---
+++ .config/nvim/lua/plugins/nvim-web-devicons.lua
@@ -0,0 +1,41 @@
+return {
+  'nvim-tree/nvim-web-devicons',
+  dependencies = {
+    'rachartier/tiny-devicons-auto-colors.nvim',
+  },
+  lazy = true,
+  opts = {
+    override = {
+      zsh = {
+        icon = '',
+        color = '#428850',
+        cterm_color = '65',
+        name = 'Zsh',
+      },
+    },
+    color_icons = true,
+    variant = 'dark',
+    override_by_filename = {
+      ['.gitignore'] = {
+        icon = '',
+        color = '#f1502f',
+        name = 'Gitignore',
+      },
+    },
+    override_by_extension = {
+      ['log'] = {
+        icon = '',
+        color = '#81e043',
+        name = 'Log',
+      },
+    },
+    override_by_operating_system = {
+      ['apple'] = {
+        icon = '',
+        color = '#A2AAAD',
+        cterm_color = '248',
+        name = 'Apple',
+      },
+    },
+  },
+}
