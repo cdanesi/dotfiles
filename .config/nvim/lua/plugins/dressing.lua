7ca281eaaca7b502ee4817fae283335a8aa8397f
Charles Danesi <charles@charlesdanesi.net>
2024-11-25 09:15:30 -0500

    add select options

--- .config/nvim/lua/plugins/dressing.lua
+++ .config/nvim/lua/plugins/dressing.lua
@@ -1,36 +1,52 @@
 return {
-  'stevearc/dressing.nvim',
-  dependencies = {
-    'nvim-telescope/telescope.nvim',
-  },
-  event = 'VeryLazy',
-  opts = {
-    input = {
-      enabled = true,
-      -- default_prompt = 'Prompt',
-      prompt_align = 'left',
-      insert_only = false,
-      start_in_insert = true,
-      border = 'rounded',
-      relative = 'cursor',
-      prefer_width = 0.4,
-      mappings = {
-        n = {
-          ['<Esc>'] = 'Close',
-          ['q'] = 'Close',
-          ['<CR>'] = 'Confirm',
-        },
-        i = {
-          ['<C-c>'] = 'Close',
-          ['<CR>'] = 'Confirm',
-          ['<Up>'] = 'HistoryPrev',
-          ['<Down>'] = 'HistoryNext',
-        },
+   'stevearc/dressing.nvim',
+   dependencies = {
+      'nvim-telescope/telescope.nvim',
+   },
+   event = 'VeryLazy',
+   opts = {
+      input = {
+         enabled = true,
+         -- default_prompt = 'Prompt',
+         prompt_align = 'left',
+         insert_only = false,
+         start_in_insert = true,
+         border = 'rounded',
+         relative = 'cursor',
+         prefer_width = 0.4,
+         mappings = {
+            n = {
+               ['<Esc>'] = 'Close',
+               ['q'] = 'Close',
+               ['<CR>'] = 'Confirm',
+            },
+            i = {
+               ['<C-c>'] = 'Close',
+               ['<CR>'] = 'Confirm',
+               ['<Up>'] = 'HistoryPrev',
+               ['<Down>'] = 'HistoryNext',
+            },
+         },
+         override = function(conf)
+            return conf
+         end,
       },
-      override = function(conf)
-        return conf
-      end,
-    },
-    select = {},
-  },
+      select = {
+         enabled = true,
+         backend = { 'telescope', 'builtin', 'fzf_lua', 'fzf', 'nui' },
+         trim_prompt = true,
+         builtin = {
+            border = 'rounded',
+            relative = 'cursor',
+            mappings = {
+               ['Esc'] = 'Close',
+               ['q'] = 'Close',
+               ['<CR>'] = 'Confirm',
+            },
+         },
+         override = function(conf)
+            return conf
+         end,
+      },
+   },
 }
