96bcf5d85020d540c903bd624d723cf0798612b0
Charles Danesi <charles@charlesdanesi.net>
2024-11-24 19:42:22 -0500

    add yank highlight from raymon-roos

--- .config/nvim/lua/core/autocmds.lua
+++ .config/nvim/lua/core/autocmds.lua
@@ -1,7 +1,23 @@
-vim.api.nvim_create_autocmd("FileType", {
-   pattern = "markdown",
-   callback = function()
-      vim.opt_local.textwidth = 80
-      vim.opt_local.spell = true
-   end,
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+
+--  ──────────────────[ set options for markdown files ]───────────────
+autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    vim.opt_local.textwidth = 80
+    vim.opt_local.spell = true
+  end,
+})
+
+-- ┌
+-- │ Highlight on yank
+-- │ from https://github.com/raymon-roos/neovim-config/blob/main/lua/core/autocmds.lua
+-- └
+augroup('YankHighlight', { clear = true })
+autocmd('TextYankPost', {
+  group = 'YankHighlight',
+  callback = function()
+    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = '800' })
+  end,
 })
