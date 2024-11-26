4663c8c36812391543e4d733f0808d01b3c487a7
Charles Danesi <charles@charlesdanesi.net>
2024-11-26 16:31:25 -0500

    style(icons): add more custom icons for file browser

--- .config/nvim/lua/plugins/nvim-web-devicons.lua
+++ .config/nvim/lua/plugins/nvim-web-devicons.lua
@@ -1,46 +1,58 @@
 return {
-  'nvim-tree/nvim-web-devicons',
-  dependencies = {
-    'rachartier/tiny-devicons-auto-colors.nvim',
-  },
-  lazy = true,
-  opts = {
-    override = {
-      zsh = {
-        icon = '',
-        color = '#428850',
-        cterm_color = '65',
-        name = 'Zsh',
+   'nvim-tree/nvim-web-devicons',
+   dependencies = {
+      'rachartier/tiny-devicons-auto-colors.nvim',
+   },
+   lazy = true,
+   opts = {
+      override = {
+         zsh = {
+            icon = '',
+            color = '#428850',
+            cterm_color = '65',
+            name = 'Zsh',
+         },
       },
-    },
-    color_icons = true,
-    variant = 'dark',
-    override_by_filename = {
-      ['.gitignore'] = {
-        icon = '',
-        color = '#f1502f',
-        name = 'Gitignore',
+      color_icons = true,
+      variant = 'dark',
+      override_by_filename = {
+         ['.gitignore'] = {
+            icon = '',
+            color = '#f1502f',
+            name = 'Gitignore',
+         },
+         ['nvim'] = {
+            icon = '',
+            name = 'nvim',
+            color = '#a3be8c',
+         },
+         ['init.lua'] = {
+            icon = '',
+            name = 'nvim',
+            color = '#a3be8c',
+         },
+         ['plugins'] = {
+            icon = '󰚥',
+         },
       },
-      ['init.lua'] = {
-        icon = '',
-        name = 'nvim',
-        color = '#a3be8c',
+      override_by_extension = {
+         ['log'] = {
+            icon = '',
+            color = '#81e043',
+            name = 'Log',
+         },
+         ['sh'] = {
+            icon = '󱆃',
+            name = 'bash',
+         },
       },
-    },
-    override_by_extension = {
-      ['log'] = {
-        icon = '',
-        color = '#81e043',
-        name = 'Log',
+      override_by_operating_system = {
+         ['apple'] = {
+            icon = '',
+            color = '#A2AAAD',
+            cterm_color = '248',
+            name = 'Apple',
+         },
       },
-    },
-    override_by_operating_system = {
-      ['apple'] = {
-        icon = '',
-        color = '#A2AAAD',
-        cterm_color = '248',
-        name = 'Apple',
-      },
-    },
-  },
+   },
 }
