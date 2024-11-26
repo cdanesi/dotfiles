6007bdc16ef35c0257d17c4a8549356ba63de739
Charles Danesi <charles@charlesdanesi.net>
2024-11-25 21:47:52 -0500

    prevent lazy loading

--- .config/nvim/lua/plugins/auto-session.lua
+++ .config/nvim/lua/plugins/auto-session.lua
@@ -1,25 +1,25 @@
 return {
-  'rmagatti/auto-session',
-  -- lazy = false,
-  event = 'VeryLazy',
+   'rmagatti/auto-session',
+   lazy = false,
 
-  ---@module "auto-session"
-  ---@type AutoSession.Config
-  opts = {
-    auto_save = true,
-    auto_restore = false, -- don't auto restore the last session
-    session_lens = {
-      load_on_setup = true,
-      previewer = false,
+   ---@module "auto-session"
+   ---@type AutoSession.Config
+   opts = {
+      auto_save = true,
+      auto_restore = false, -- don't auto restore the last session
+      lazy_support = true,
+      session_lens = {
+         load_on_setup = true,
+         previewer = false,
 
-      mappings = {
-        delete_session = { 'i', '<C-D>' },
-        alternate_session = { 'i', '<C-S>' },
-        copy_session = { 'i', '<C-Y>' },
+         mappings = {
+            delete_session = { 'i', '<C-D>' },
+            alternate_session = { 'i', '<C-S>' },
+            copy_session = { 'i', '<C-Y>' },
+         },
       },
-    },
-    bypass_save_filetypes = { 'alpha', 'dashboard' }, -- don't save dashboard/greeter to a session
-    -- Folders listed here will not have sessions created within.
-    suppress_dirs = { '~/', '~/Downloads', '~/Documents', '~/Desktop', '/' },
-  },
+      bypass_save_filetypes = { 'alpha', 'dashboard' }, -- don't save dashboard/greeter to a session
+      -- Folders listed here will not have sessions created within.
+      suppress_dirs = { '~/', '~/Downloads', '~/Desktop', '/' },
+   },
 }
