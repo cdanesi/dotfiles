b49c888641ecc9b54bfd56996745757429766554
Charles Danesi <charles@charlesdanesi.net>
2024-11-29 14:13:34 -0500

    trouble no longer gains focus immediately after opening

--- .config/nvim/lua/plugins/trouble.lua
+++ .config/nvim/lua/plugins/trouble.lua
@@ -1,11 +1,11 @@
 return {
-  'folke/trouble.nvim',
-  dependencies = {
-    'nvim-tree/nvim-web-devicons',
-    'folke/todo-comments.nvim',
-  },
-  cmd = 'Trouble',
-  opts = {
-    focus = true,
-  },
+   'folke/trouble.nvim',
+   dependencies = {
+      'nvim-tree/nvim-web-devicons',
+      'folke/todo-comments.nvim',
+   },
+   cmd = 'Trouble',
+   opts = {
+      focus = false,
+   },
 }
