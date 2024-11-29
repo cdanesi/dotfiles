fffcadf0ce5ed425aa11aaae726fff0b5fad8c0a
Charles Danesi <charles@charlesdanesi.net>
2024-11-29 17:57:17 -0500

    chore: update disabled plugins

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -1,26 +1,39 @@
 return {
-  {
-    'ThePrimeagen/harpoon',
-    enabled = false,
-  },
-  {
-    'tadmccorkle/markdown.nvim',
-    enabled = false,
-  },
-  {
-    'OXY2DEV/markview.nvim',
-    enabled = false,
-  },
-  {
-    'jakewvincent/mkdnflow.nvim',
-    enabled = false,
-  },
-  {
-    'willothy/moveline.nvim',
-    enabled = false,
-  },
-  {
-    'MeanderingProgrammer/render-markdown.nvim',
-    enabled = false,
-  },
+   {
+      'renerocksai/telekasten.nvim',
+      enabled = false,
+   },
+   {
+      'ThePrimeagen/harpoon',
+      enabled = false,
+   },
+   {
+      'tadmccorkle/markdown.nvim',
+      enabled = false,
+   },
+   {
+      'OXY2DEV/markview.nvim',
+      enabled = false,
+   },
+   {
+      'jakewvincent/mkdnflow.nvim',
+      enabled = false,
+   },
+   {
+      'willothy/moveline.nvim',
+      enabled = false,
+   },
+   {
+      'MeanderingProgrammer/render-markdown.nvim',
+      enabled = false,
+   },
+   {
+      'JoosepAlviste/nvim-ts-context-commentstring',
+      dependencies = { 'numToStr/Comment.nvim' },
+      event = 'VeryLazy',
+      opts = {
+         enable_autocmd = false,
+      },
+      enabled = false,
+   },
 }
