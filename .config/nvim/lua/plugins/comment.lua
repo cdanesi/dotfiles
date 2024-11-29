9015900222ad3008f579dd7f46a17b0ef4c4830c
Charles Danesi <charles@charlesdanesi.net>
2024-11-29 17:56:10 -0500

    style: indents and spacing
    add default configuration options

--- .config/nvim/lua/plugins/comment.lua
+++ .config/nvim/lua/plugins/comment.lua
@@ -1,16 +1,30 @@
 return {
-   "numToStr/Comment.nvim",
-   event = { "BufReadPre", "BufNewFile" },
-   dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
+   'numToStr/Comment.nvim',
+   event = { 'BufReadPre', 'BufNewFile' },
+   opts = {
+      padding = true,
+      sticky = true,
+      toggler = {
+         line = 'gcc',
+         block = 'gbc',
+      },
+      opleader = {
+         line = 'gc',
+         block = 'gb',
+      },
+      extra = {
+         -- Add comment on the line above
+         above = 'gcO',
+         -- Add comment on the line below
+         below = 'gco',
+         -- Add comment at the end of line
+         eol = 'gcA',
+      },
+      mappings = {
+         basic = true,
+         extra = true,
+      },
+      pre_hook = nil,
+      post_hook = nil,
    },
-
-   config = function()
-      local comment = require("Comment")
-      local ts_context_commentstring = require("ts_context_commentstring.integrations.comment_nvim")
-
-      comment.setup({
-         pre_hook = ts_context_commentstring.create_pre_hook(),
-      })
-   end,
 }
