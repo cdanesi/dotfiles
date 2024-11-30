f97b157efb0b41f570c7363e729d305fd5cca36d
Charles Danesi <charles@charlesdanesi.net>
2024-11-29 23:57:13 -0500

    style: changed look of which-key popup window

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -1,10 +1,26 @@
 return {
-   "folke/which-key.nvim",
-   event = "VeryLazy",
+   'folke/which-key.nvim',
+   event = 'VeryLazy',
    init = function()
       vim.o.timeout = true
       vim.o.timeoutlen = 500
    end,
    opts = {
+      preset = 'modern',
+      sort = { 'local', 'order', 'group', 'alphanum', 'mod' },
+      notify = true,
+      win = {
+         title = true,
+         title_pos = 'center',
+         padding = { 2, 3 },
+      },
+      plugins = {
+         marks = true,
+         registers = true,
+         spelling = {
+            enabled = true,
+            suggestions = 20,
+         },
+      },
    },
 }
