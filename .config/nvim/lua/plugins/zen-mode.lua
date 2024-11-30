beea8e552505bbdb0a2b219525fad171ea51255c
Charles Danesi <charles@charlesdanesi.net>
2024-11-29 23:29:08 -0500

    chore: width adjustment

--- .config/nvim/lua/plugins/zen-mode.lua
+++ .config/nvim/lua/plugins/zen-mode.lua
@@ -1,25 +1,29 @@
 return {
-  'folke/zen-mode.nvim',
-  dependencies = {
-    'folke/twilight.nvim',
-  },
-  cmd = 'ZenMode',
-  opts = {
-    window = { signcolumn = false },
-    plugins = {
-      options = { enabled = true },
-      twilight = { enabled = true },
-      gitsigns = { enabled = true },
-      tmux = { enabled = true },
-      todo = { enabled = true },
-      alacritty = {
-        enabled = true,
-        font = '16',
+   'folke/zen-mode.nvim',
+   dependencies = {
+      'folke/twilight.nvim',
+   },
+   cmd = 'ZenMode',
+   opts = {
+      window = {
+         width = 0.75,
+         signcolumn = false,
       },
-      kitty = {
-        enabled = true,
-        font = '+4',
+      plugins = {
+         options = { enabled = true },
+         twilight = { enabled = true },
+         gitsigns = { enabled = true },
+         tmux = { enabled = true },
+         todo = { enabled = true },
+         alacritty = {
+            enabled = true,
+            font = '16',
+         },
+         kitty = {
+            -- INFO: not working in tmux sessions
+            enabled = true,
+            font = '+4',
+         },
       },
-    },
-  },
+   },
 }
