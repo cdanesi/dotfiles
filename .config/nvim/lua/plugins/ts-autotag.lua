1060f80f5f1aeaa365c4ad901351ac47081eb4cf
Charles Danesi <charles@charlesdanesi.net>
2024-12-01 07:23:59 -0500

    feat: add autotag support for treesitter

---
+++ .config/nvim/lua/plugins/ts-autotag.lua
@@ -0,0 +1,25 @@
+return {
+   'windwp/nvim-ts-autotag',
+   ft = {
+      'astro',
+      'glimmer',
+      'handlebars',
+      'html',
+      'javascript',
+      'jsx',
+      'markdown',
+      'php',
+      'rescript',
+      'svelte',
+      'tsx',
+      'twig',
+      'typescript',
+      'vue',
+      'xml',
+   },
+   opts = {
+      opts = {
+         enable_close_on_slash = true,
+      },
+   },
+}
