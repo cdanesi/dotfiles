81a4767d15fce50dd60f7da66d999650ef513aeb
Charles Danesi <charles@charlesdanesi.net>
2024-12-01 07:17:32 -0500

    chore: correct typo in comment

--- .config/nvim/lua/plugins/twilight.lua
+++ .config/nvim/lua/plugins/twilight.lua
@@ -1,15 +1,15 @@
 return {
-  'folke/twilight.nvim',
-  lazy = true,
-  opts = {
-    context = 10,
-    treesitter = true,
-    expand = {
-      'function',
-      'method',
-      'table',
-      'if_statement',
-    },
-    exclude = {}, -- filetypes to excluise
-  },
+   'folke/twilight.nvim',
+   lazy = true,
+   opts = {
+      context = 10,
+      treesitter = true,
+      expand = {
+         'function',
+         'method',
+         'table',
+         'if_statement',
+      },
+      exclude = {}, -- filetypes to exclude
+   },
 }
