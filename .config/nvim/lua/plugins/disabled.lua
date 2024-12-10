a768189dd3c8e65a42cf8d0da530a3ae9dff0417
Charles Danesi <charles@charlesdanesi.net>
2024-12-10 14:47:05 -0500

    chore: update plugin versions and disabled list

--- .config/nvim/lua/plugins/disabled.lua
+++ .config/nvim/lua/plugins/disabled.lua
@@ -9,16 +9,30 @@ return {
       'ziontee113/icon-picker.nvim',
       enabled = false,
    },
-   {
-      -- TODO: configure this properly
-      'ThePrimeagen/harpoon',
-      enabled = false,
-   },
-   {
+   --[[ {
       -- TODO: decide between this, obsidian, and mkdnflow
       'renerocksai/telekasten.nvim',
       enabled = false,
-   },
+      dependencies = { 'nvim-telescope/telescope.nvim' },
+      event = 'VeryLazy',
+      opts = {
+         vaults = {
+            personal = {
+               home = '~/vaults',
+            },
+            writing = {
+               home = '~/notes',
+            },
+         },
+      },
+
+      config = function(_, opts)
+         require('telekasten').setup({ opts })
+      end,
+      -- require('telekasten').setup({
+      --    home = vim.fn.expand('~/vault'), -- Put the name of your notes directory here
+      -- }),
+   }, ]]
    {
       'epwalsh/obsidian.nvim',
       enabled = false,
@@ -36,23 +50,124 @@ return {
       -- torn between this and render-markdown,
       'OXY2DEV/markview.nvim',
       enabled = false,
+      dependencies = {
+         'nvim-treesitter/nvim-treesitter',
+         'nvim-tree/nvim-web-devicons',
+      },
+      lazy = false,
+      ft = 'markdown',
    },
    {
       'MeanderingProgrammer/render-markdown.nvim',
       enabled = false,
    },
    {
+      'nvim-telescope/telescope-frecency.nvim',
+      enabled = false,
+      dependencies = {
+         'nvim-telescope/telescope.nvim',
+      },
+      config = function()
+         require('telescope').setup({
+            extensions = {
+               frecency = {
+                  show_scores = true,
+                  -- matcher = "fuzzy",
+                  db_safe_mode = false,
+                  auto_validate = true,
+                  db_validate_threshold = 10,
+                  show_filter_column = false,
+                  ignore_patterns = { '*/.git', '*/.git/*', '*/.DS_Store' },
+                  workspaces = {
+                     ['dots'] = '$HOME/.config',
+                     ['proj'] = '$HOME/projects',
+                     ['ansible'] = '$HOME/projects/ansible',
+                     ['docker'] = '$HOME/projects/docker',
+                     ['code'] = '$HOME/projects/code',
+                  },
+               },
+            },
+         })
+         require('telescope').load_extension('frecency')
+      end,
+   },
+   {
       -- couldn't get this working..
       'JoosepAlviste/nvim-ts-context-commentstring',
+      enabled = false,
       dependencies = { 'numToStr/Comment.nvim' },
       event = 'VeryLazy',
       opts = {
          enable_autocmd = false,
       },
+   },
+   {
+      'mbbill/undotree',
       enabled = false,
+      cmd = { 'UndotreeToggle', 'UndotreeShow', 'UndotreeHide' },
+
+      config = function()
+         local undotree = require('undotree')
+
+         undotree.setup()
+      end,
    },
    {
       'debugloop/telescope-undo.nvim',
       enabled = false,
+      dependencies = {
+         {
+            'nvim-telescope/telescope.nvim',
+            dependencies = { 'nvim-lua/plenary.nvim' },
+         },
+      },
+      keys = {
+         {
+            '<leader>u',
+            '<cmd>Telescope undo<cr>',
+            desc = 'Undo history',
+         },
+      },
+      opts = {
+         extensions = {
+            undo = {
+               use_delta = true,
+               side_by_side = true,
+               saved_only = false,
+               layout_strategy = 'vertical',
+               layout_config = {
+                  preview_height = 0.8,
+               },
+               mappings = {
+                  i = {
+                     ['<C-y>'] = function(bufnr)
+                        return require('telescope-undo.actions').yank_additions(bufnr)
+                     end,
+                     ['<C-Y>'] = function(bufnr)
+                        return require('telescope-undo.actions').yank_deletions(bufnr)
+                     end,
+                     ['<C-cr>'] = function(bufnr)
+                        return require('telescope-undo.actions').restore(bufnr)
+                     end,
+                  },
+                  n = {
+                     ['y'] = function(bufnr)
+                        return require('telescope-undo.actions').yank_additions(bufnr)
+                     end,
+                     ['Y'] = function(bufnr)
+                        return require('telescope-undo.actions').yank_deletions(bufnr)
+                     end,
+                     ['u'] = function(bufnr)
+                        return require('telescope-undo.actions').restore(bufnr)
+                     end,
+                  },
+               },
+            },
+         },
+      },
+      config = function(_, opts)
+         require('telescope').setup(opts)
+         require('telescope').load_extension('undo')
+      end,
    },
 }
