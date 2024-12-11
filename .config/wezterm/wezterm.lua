16d9a9f2bece4a69ba1e42d3ed0b01b57c743357
Charles Danesi <charles@charlesdanesi.net>
2024-12-11 10:11:48 -0500

    update wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,8 +1,27 @@
 local wezterm = require("wezterm")
-local config = {
-	color_scheme = "Nord",
-	-- font = wezterm.font("JetBrainsMono"),
-	font_size = 11,
+local config = wezterm.config_builder()
+
+config = {
+	term = "xterm-kitty",
+	enable_kitty_graphics = true,
+	max_fps = 60,
+	color_scheme = "nord",
+	window_decorations = "RESIZE",
+	enable_tab_bar = false,
+	default_cursor_style = "SteadyBlock",
+	cursor_blink_ease_out = "Constant",
+	cursor_blink_ease_in = "Constant",
+	cursor_blink_rate = 0,
+	font = wezterm.font("JetBrainsMono Nerd Font"),
+	font_size = 14,
+	window_background_opacity = 0.6,
+	macos_window_background_blur = 15,
+	window_padding = {
+		left = 2,
+		right = 2,
+		top = 15,
+		bottom = 0,
+	},
 }
 
 return config
