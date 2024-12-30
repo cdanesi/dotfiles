e1ccd6c77f80c5e4fa19a38df250e727107d2429
Charles Danesi <charles@charlesdanesi.net>
2024-12-30 14:54:43 -0500

    fix: remove prettier as formatter for md

--- .config/nvim/lua/plugins/prettier.lua
+++ .config/nvim/lua/plugins/prettier.lua
@@ -12,7 +12,7 @@ return {
          html = { 'prettier' },
          json = { 'prettier' },
          yaml = { 'prettier' },
-         markdown = { 'markdownlint', 'prettier' },
+         markdown = { 'markdownlint' },
          graphql = { 'prettier' },
          liquid = { 'prettier' },
          lua = { 'stylua' },
