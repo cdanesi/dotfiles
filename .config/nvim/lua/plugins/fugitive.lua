db620f033c8c0e41692b916cd1a63dbd441ec0be
Charles Danesi <charles@charlesdanesi.net>
2025-08-29 20:56:49 -0400

    add yadm to fugitive config

--- .config/nvim/lua/plugins/fugitive.lua
+++ .config/nvim/lua/plugins/fugitive.lua
@@ -1,5 +1,14 @@
 return {
    'tpope/vim-fugitive',
+   dependencies = {
+      'purarue/yadm-git.vim',
+      config = function()
+         vim.g.yadm_git_enabled = 1
+         vim.g.yadm_git_fugitive_enabled = 1
+         vim.g.yadm_git_gutter_enabled = 1
+         vim.g.yadm_repo_path = '~/.local/share/yadm/repo.git/'
+      end
+   },
    cmd = {
       'Git',
       'Gdiffsplit',
